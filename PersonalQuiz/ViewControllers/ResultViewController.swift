a552d35bcc6cf01309cc85fddcd480e541f4c75c
Maxim Makarov <makarovmaxqa@gmail.com>
2023-12-04 23:56:52 +0100

    Hide back button

--- PersonalQuiz/ViewControllers/ResultViewController.swift
+++ PersonalQuiz/ViewControllers/ResultViewController.swift
@@ -9,8 +9,15 @@ import UIKit
 
 class ResultViewController: UIViewController {
 
+    @IBOutlet var animalTypeLabel: UILabel!
+    @IBOutlet var descriptionLabel: UILabel!
+    
+    var answers: [Answer]!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        navigationItem.hidesBackButton = true
     }
     
     @IBAction func doneButtonAction(_ sender: UIBarButtonItem) {
